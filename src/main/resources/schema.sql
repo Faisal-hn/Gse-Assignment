d16ee70cd1b0245b5ba0f8f2f22e97ac5a78172d
Faisal-hn <hayat.khanum92@gmail.com>
2025-02-01 20:58:01 +0530

    dockercompose file configuration updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA IF NOT EXISTS mydatabase;
+CREATE SCHEMA IF NOT EXISTS gse_cart;
 
-CREATE TABLE IF NOT EXISTS mydatabase.contacts (
+CREATE TABLE IF NOT EXISTS gse_cart.contacts (
     id BIGINT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     phone BIGINT NOT NULL,
     primary_id BIGINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (primary_id) REFERENCES mydatabase.contacts(id)
+    FOREIGN KEY (primary_id) REFERENCES gse_cart.contacts(id)
 );
